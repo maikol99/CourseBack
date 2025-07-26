691a0ff01fd7c9740decc0608fcd3707ec845468
Maikol Alegre <alegremaikol21@gmail.com>
2025-07-26 20:47:30 -0300

    creacion componente toast para alertas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button } from "./components/ui/Button";
+import Navbar from "./pages/Navbar";
+import { Input } from "./components/ui/input";
+import "./App.css";
+import { Textarea } from "./components/ui/textarea";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const addTodoHandler = () =>{
+    console.log(title,description);
+  }
 
   return (
     <div>
-      <button>lets build to do App 🔥🔥</button>
+      <Navbar />
+      <div className="flex items-center gap-5 mt-5">
+        <Input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Agregar nueva tarea..."
+          className="w-1/4 mt-2"
+        />
+        <Button onClick={addTodoHandler}>Agregar Tarea 🚀</Button>
+          <Textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Agregar descripcion..."
+            className="w-1/4 mt-2"
+          />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
